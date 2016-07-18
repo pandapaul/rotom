76cb3304187c5ef275ba6a64800d58bce8d08f2d
Sergio Rodriguez <oneandprime@gmail.com>
2016-07-17 23:19:01 -0400

    Refactor

--- server.js
+++ server.js
@@ -1,51 +1,13 @@
 var express = require('express'),
 	router = express.Router(),
-	request = require('request'),
+	api = require('./api/index'),
 	app = express();
 
 routes();
 listen();
 
 function routes() {
-	app.get('/', function (req, res) {
-		var pokemon = req && req.query && req.query.text && req.query.text.toLowerCase(),
-			url = 'http://pokeapi.co/api/v2/pokemon/' + pokemon;
-			
-		if(!pokemon || pokemon === '') {
-			res.json({
-				response_type: 'ephemeral',
-				text: 'Type /rotom the_name_of_a_pokémon to report that a pokémon is nearby.'
-			});
-			return;
-		}
-
-		request(url, function (error, response, body) {
-			var slackMessage;
-
-			if (!error && response.statusCode == 200) {
-				var body = JSON.parse(response.body);
-				slackMessage = {
-		    		response_type: 'in_channel',
-				    attachments: [
-				        {	
-				        	text:"",
-				            image_url: body.sprites.front_default
-				        },{	
-				        	mrkdwn_in: ["text"],
-				        	text: 'A wild *' + body.name.toUpperCase() + '* appeared!',
-				        }
-				    ]
-				};
-			} else {
-				slackMessage = {
-					'response_type': 'in_channel',
-					'text': 'Someone thought they saw a *' + pokemon.toUpperCase() + '*, but I\'m not sure what that is.'
-				};
-			}
-		    res.json(slackMessage);
-		});
-
-	});
+	app.use('/', api);
 }
 
 function listen() {
