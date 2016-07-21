f4971498344987f6976ea5d9da4cdc86ef0585d7
Sergio Rodriguez <oneandprime@gmail.com>
2016-07-20 22:15:58 -0400

    Updated fuzzy search

--- api/slack.js
+++ api/slack.js
@@ -10,55 +10,63 @@ router.get('/', function (req, res) {
 	var input = req && req.query && req.query.text && req.query.text.toLowerCase(),
 		target;
 
-	if(process.env.CHANNEL_ID && req.query.channel_id !== process.env.CHANNEL_ID) {
+	function isNotAuthorizedChannel() {
+		return !!(process.env.CHANNEL_ID && req.query.channel_id !== process.env.CHANNEL_ID);
+	} 
+
+	function sendUnathorizedChannelMessage() {
 		res.json({
 			response_type: 'ephemeral',
 			text: 'Let\'s keep this stuff where it belongs.'
-		});
-		return;
+		});		
 	}
 
-	if(!input || input === '') {
+	function isHelpInput() {
+		return !!(!input || input === '');
+	} 
+
+	function sendHelpResponse() {
 		res.json({
 			response_type: 'ephemeral',
 			text: 'Type /rotom the_name_of_a_pokémon to report that a pokémon is nearby.'
 		});
-		return;
 	}
 
-	if(input === 'game') {
+	function isGameInput() {
+		return input.toLowerCase() === 'game';
+	}
+
+	function sendGameResponse() {
 		var id = getRandomInt(1,pokemon.length);
 
 		res.json({
     		response_type: 'in_channel',
+    		text: 'Who\'s that Pokémon?',
 		    attachments: [
 		        {	
 		        	text:'',
 		            image_url: 'http://rotom.herokuapp.com/sprites/' + id + '.shadow.png'
-		        },{	
-		        	mrkdwn_in: ['text'],
-		        	text: 'Who\'s That Pokemon?',
 		        }
 		    ]
-		});		
-		res.json()
-		return;
+		});
 	}
 
-	target = pokemon.find(function (poke) {
-		return poke.name === input;
-	});
+	function getExactPokemonMatch() {
+		return pokemon.find(function (poke) {
+			return poke.name === input;
+		});
+	}
 
-	if(!target) {
-		var f = new fuse(pokemon, {keys: ['name']}),
+	function getFuzzyPokemonMatch() {
+		var f = new fuse(pokemon, {keys: ['name'], threshold: 0.8}),
 			matches = f.search(input);
 
 		if(matches && matches.length > 0) {
-			target = matches[0];
+			return matches[0];
 		}
 	}
 
-	if (target) {
+	function sendPokemonFoundResponse() {
 		res.json({
     		response_type: 'in_channel',
 		    attachments: [
@@ -67,17 +75,46 @@ router.get('/', function (req, res) {
 		            image_url: 'http://rotom.herokuapp.com/sprites/' + target.id + '.png'
 		        },{	
 		        	mrkdwn_in: ["text"],
-		        	text: 'A wild *' + target.name.toUpperCase() + '* appeared!',
+		        	text: 'A wild *' + target.name.toUpperCase() + '* appeared!'
 		        }
 		    ]
 		});
+	}
+
+	function sendNoMatchFoundResponse() {
+		res.json({
+			'response_type': 'in_channel',
+			'text': 'Someone thought they saw a *' + input.toUpperCase() + '*, but I\'m not sure what that is.'
+		});
+	}
+
+	if(isNotAuthorizedChannel()) {
+		sendUnathorizedChannelMessage()
 		return;
 	}
 
-	res.json({
-		'response_type': 'in_channel',
-		'text': 'Someone thought they saw a *' + input.toUpperCase() + '*, but I\'m not sure what that is.'
-	});
+	if(isHelpInput()) {
+		sendHelpResponse();
+		return;
+	}
+
+	if(isGameInput()) {
+		sendGameResponse();
+		return;
+	}
+
+	target = getExactPokemonMatch()
+
+	if(!target) {
+		target = getFuzzyPokemonMatch();
+	}
+
+	if (target) {
+		sendPokemonFoundResponse();
+		return;
+	}
+
+	sendNoMatchFoundResponse();
 });
 
 module.exports = router;
