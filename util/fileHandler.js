d61c51a29e8e21c224bdf9ab0eefaba2558ac60c
Sergio Rodriguez <oneandprime@gmail.com>
2016-07-21 21:07:16 -0400

    I always forget to commit all of the files

---
+++ util/fileHandler.js
@@ -0,0 +1,32 @@
+var http = require('http'),
+  fs = require('fs');
+
+function copyFile(source, target, cb) {
+  var file = fs.createWriteStream(target);
+  var request = http.get(source, function(response) {
+    response.pipe(file);
+    if(cb) {
+      cb()
+    };
+  });
+}
+
+function makeShadowCopy(src, trgt) {
+	jimp.read(src, function (err, image) {
+		if(image) {
+			image.brightness(-1);
+			image.write(trgt);
+		}
+	});	
+}
+
+function base64Encode(src) {
+    var bitmap = fs.readFileSync(src);
+    return new Buffer(bitmap).toString('base64');
+}
+
+module.exports = {
+	copyFile: copyFile,
+	makeShadowCopy: makeShadowCopy,
+	base64Encode: base64Encode
+};
