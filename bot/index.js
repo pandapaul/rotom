7bb680baab7bac20481cf3d0ea727001dfdab984
Sergio Rodriguez <oneandprime@gmail.com>
2016-07-26 21:59:10 -0400

    Added message listener

--- bot/index.js
+++ bot/index.js
@@ -10,3 +10,15 @@ var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
   console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
 });
+
+
+
+var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+
+rtm.on(RTM_EVENTS.MESSAGE, function (message) {
+  console.log(message);
+});
+
+rtm.on(RTM_EVENTS.CHANNEL_CREATED, function (message) {
+  // Listens to all `channel_created` events from the team
+});
