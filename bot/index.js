e8b716c3936046b6fddda1fefbf65839224502c6
Sergio Rodriguez <oneandprime@gmail.com>
2016-07-26 22:12:38 -0400

    Send more complicated message

--- bot/index.js
+++ bot/index.js
@@ -11,12 +11,13 @@ rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
   console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
 });
 
-
-
 var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 
 rtm.on(RTM_EVENTS.MESSAGE, function (message) {
-	rtm.sendMessage('I heard that.', message.channel);
+		rtm.sendMessage({
+	    "type": "message",
+	    "text": "Hello"
+	}, message.channel);
 });
 
 rtm.on(RTM_EVENTS.CHANNEL_CREATED, function (message) {
